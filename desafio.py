f08684c4c163cfe605a3cb932328743af650f79a
Italo Eduardo <italoeot@gmail.com>
2024-12-03 19:37:38 -0300

    Finalização de desafio aula 03

--- desafio.py
+++ desafio.py
@@ -1,36 +1,50 @@
 base_ano = 1000
+nome_valido = False
+salario_valido = False
+bonus_valido = False
 
-try:
-    nome_funcionario = input("Digite o seu nome: ")
-    # Verifica se o nome foi preenchido
-    if len(nome_funcionario) == 0:
-        print("Nome não preenchido. Gentileza inserir o seu nome para realização de cálculo.")
-    # Verifica se nome foi preenchido somente com espaços em branco
-    elif nome_funcionario.isspace() == True:
-        print("Nome preenchido somente com espaços. Realizar entrada válida.")
-    # Verificação se houve inserção de números no campo string
-    elif any(char.isdigit() for char in nome_funcionario):
-        print("Entrada não aceita números. Inserir nome válido")
-    else:
-        print("Nome inserido com sucesso!")
-except ValueError as e:
-    print(e)
+while not nome_valido:
+    try:
+        nome_funcionario = input("Digite o seu nome: ")
+        # Verifica se o nome foi preenchido
+        if len(nome_funcionario) == 0:
+            print("Nome não preenchido. Gentileza inserir o seu nome para realização de cálculo.")
+        # Verifica se nome foi preenchido somente com espaços em branco
+        elif nome_funcionario.isspace() == True:
+            print("Nome preenchido somente com espaços. Realizar entrada válida.")
+        # Verificação se houve inserção de números no campo string
+        elif any(char.isdigit() for char in nome_funcionario):
+            print("Entrada não aceita números. Inserir nome válido")
+        else:
+            print(f"Nome válido: {nome_funcionario}")
+            print("Nome inserido com sucesso!")
+            nome_valido = True
+    except ValueError as e:
+        print(e)
 
+while not salario_valido:
     # Verificação para inserção de salários
-try:
-    salario = float(input("Digite o seu salário: "))
-    if salario < 0:
-        print("Salário inserido incorretamente. Favor inserir valor positivo.")
-except ValueError:
-    print("Entrada inválida.")
+    try:
+        salario = float(input("Digite o seu salário: "))
+        if salario < 0:
+            print("Salário inserido incorretamente. Favor inserir valor positivo.")
+        else:
+            salario_valido = True
+    except ValueError:
+        print("Entrada inválida.")
+
 
     # Verificação para inserção de Bônus
-try:
-    bonus = float(input("Digite o percentual de bonus: "))
-    if bonus < 0:
-        print("Bônus inválido. Gentileza inserir valor positivo.")
-except ValueError:
-    print("Entrada inválida!")
+
+while not bonus_valido:
+    try:
+        bonus = float(input("Digite o percentual de bonus: "))
+        if bonus < 0:
+            print("Bônus inválido. Gentileza inserir valor positivo.")
+        else:
+            bonus_valido = True
+    except ValueError:
+        print("Entrada inválida!")
 
 valor_bonus = (base_ano + salario) * bonus
 print(f"Olá {nome_funcionario}, o seu bônus foi de {valor_bonus} !")
